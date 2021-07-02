fa1c476111f1906bf893f083f1d802b956ad65d3
thathinisai <thathinisai@gmail.com>
2021-07-02 19:07:15 +0200

    implemented a dog age calculator)

---
+++ javascript/javascript1/week1/goodboy-oldboy.js
@@ -0,0 +1,11 @@
+const dogYearOfBirth = 1985;
+let dogYearFuture = 2027;
+let shouldShowResultInDogYears = true;
+if (shouldShowResultInDogYears === true){
+    let dogYear = (dogYearFuture-dogYearOfBirth)*7;
+ console.log("Your dog will be" + dogYear + " dog years old in" + dogYearFuture);   
+}
+else if(shouldShowResultInDogYears === false) {
+    let humanYear = dogYearFuture-dogYearOfBirth;
+    console.log("Your dog will be" + humanYear + "human years old in" + dogYearFuture);
+}

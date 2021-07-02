62611e16e88d65009a3b316faff79535bf2a7c2e
thathinisai <thathinisai@gmail.com>
2021-07-02 19:40:37 +0200

    estimating house price

---
+++ javascript/javascript1/week1/house-price-estimator.js
@@ -0,0 +1,16 @@
+let width = 8;
+let depth = 10;
+let hieght = 10;
+let gardenSizeInM2 = 100;
+let payingHousePrice = 2500000;
+
+let volumeInMeters = width*depth*hieght;
+
+let housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+
+if(housePrice > payingHousePrice){
+    console.log("paying too much");
+}
+else if(housePrice < payingHousePrice){
+    console.log("paying too less");
+}

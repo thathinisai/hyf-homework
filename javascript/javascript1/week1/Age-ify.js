7719de0ddd750cd9d9c9febdf9188c0c6df39913
thathinisai <thathinisai@gmail.com>
2021-07-02 18:23:30 +0200

    implemented Age-ify (A future age calculator)

--- javascript/javascript1/week1/Age-ify.js
+++ javascript/javascript1/week1/Age-ify.js
@@ -1,4 +1,4 @@
 const yearOfBirth = 1985;
-const yearFuture = 2030;
-const age = yearFuture - yearOfBirth ;
-console.log("you will be" +age+ "years old in 2027");
+let yearFuture = 2040;
+let age = yearFuture - yearOfBirth;
+console.log("you will be " + age + " years old in " + yearFuture);

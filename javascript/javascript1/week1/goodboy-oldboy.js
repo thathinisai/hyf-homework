64e8d080577c7b8c3bd8a98162eea88a11221c7c
thathinisai <thathinisai@gmail.com>
2021-08-03 11:31:30 +0200

    dog years file added

--- javascript/javascript1/week1/goodboy-oldboy.js
+++ javascript/javascript1/week1/goodboy-oldboy.js
@@ -1,11 +1,10 @@
 const dogYearOfBirth = 1985;
-let dogYearFuture = 2027;
+const dogYearFuture = 2027;
 let shouldShowResultInDogYears = true;
 if (shouldShowResultInDogYears === true){
-    let dogYear = (dogYearFuture-dogYearOfBirth)*7;
- console.log("Your dog will be " + dogYear + " dog years old in" + dogYearFuture);   
+    const dogYear = (dogYearFuture-dogYearOfBirth);
+ console.log("Your dog will be " + dogYear*7 + " dog years old in" + dogYearFuture);   
 }
 else if(shouldShowResultInDogYears === false) {
-    let humanYear = dogYearFuture-dogYearOfBirth;
-    console.log("Your dog will be " + humanYear + " human years old in" + dogYearFuture);
+    console.log("Your dog will be " + dogYear + " human years old in" + dogYearFuture);
 }

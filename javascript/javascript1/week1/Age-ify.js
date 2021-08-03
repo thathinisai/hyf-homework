fa3acfc123a5bac1dd2d4c2fe1136e0c50562a32
thathinisai <thathinisai@gmail.com>
2021-08-03 11:10:31 +0200

    age file added

--- javascript/javascript1/week1/Age-ify.js
+++ javascript/javascript1/week1/Age-ify.js
@@ -1,4 +1,4 @@
 const yearOfBirth = 1985;
 let yearFuture = 2040;
 let age = yearFuture - yearOfBirth;
-console.log("you will be " + age + " years old in " + yearFuture);
+console.log(`you will be ${age}  years old in  ${yearFuture}`);

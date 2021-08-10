2347958f8e233ddc6dcd66defca8a680585fc182
thathinisai <thathinisai@gmail.com>
2021-08-10 10:05:46 +0200

    candy function  file  added

--- javascript/javascript1/week2/candyfunction.js
+++ javascript/javascript1/week2/candyfunction.js
@@ -1,12 +1,12 @@
 let boughtCandyPrices = [];
 function addCandy(candyType,weight){
-    if(candyType = "sweet"){
+    if(candyType === "sweet"){
         boughtCandyPrices.push(0.5*weight);
-    }else if(candyType = "chacolate"){
+    }else if(candyType === "chacolate"){
         boughtCandyPrices.push(0.7*weight);
-    }else if(candyType = "toffe"){
+    }else if(candyType === "toffe"){
         boughtCandyPrices.push(1.1*weight);
-    }else if(candyType = "chewing-gum"){
+    }else if(candyType === "chewing-gum"){
         boughtCandyPrices.push(0.03*weight);
     }else{
         return "invalid entry";
@@ -19,15 +19,12 @@ addCandy("chacolate",10);
 addCandy("toffe",30);
 addCandy("chewing-gum",10);
 addCandy("salt",20);
-
 let boughtCandy = 0;
-
 let amountToSpend = (Math.random() * 100);
-for (let i=0; i < boughtCandyPrices.length; i++) {
-    boughtCandy += boughtCandyPrices[i];
-}
-
 function canBuyMoreCandy(){
+    for (let i=0; i < boughtCandyPrices.length; i++) {
+    boughtCandy += boughtCandyPrices[i];
+    }
     if (amountToSpend > boughtCandy) {
         console.log("You can buy more, so please do!.");
     } else {

a4ae6c931295b641f399fa420a9f9eeb08390a4a
thathinisai <thathinisai@gmail.com>
2021-09-29 13:50:08 +0200

    weather app js   file  added

--- javascript/javascript3/week1/week1weatherapp.js
+++ javascript/javascript3/week1/week1weatherapp.js
@@ -5,46 +5,42 @@ let weatherUrl;
 
 //Check for input city
 userButton.addEventListener("click", getCurrentWeatherByCityName);
-  function getCurrentWeatherByCityName() 
-  {
-    const cityName = userCity.value;
-    const appkey = "3cde58a1dd84fa31e343be2e7ebcfb24";
-       if (!cityName) 
-        {
-          alert("Please enter city");
-        }
-        else
-        {
-            weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${appkey}`;
-            getWeather();
-        }
+function getCurrentWeatherByCityName() {
+  const cityName = userCity.value;
+  const appkey = "3cde58a1dd84fa31e343be2e7ebcfb24";
+  if (!cityName) {
+    alert("Please enter city");
+  } else {
+    weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${appkey}`;
+    getWeather();
   }
-  //getting data and display in browser
-  function getWeather()
-  {
-    fetch(weatherUrl)
+}
+//getting data and display in browser
+function getWeather() {
+  fetch(weatherUrl)
     .then((response) => response.json())
-    .then((data) => 
-    {
-        h3.textContent = "";
-        if (data.cod === "404")
-        {
-           h3.textContent = `Entered '${userCity.value}' ${data.message}`;
-        } else 
-        {
-          city.textContent = data.name;
-          const actualTemp = data.main.temp - 273;
-          const maxtemp = data.main.temp_max - 273;
-          const mintemp = data.main.temp_min - 273;
-          temperature.textContent = `Temperature: ${actualTemp.toFixed(0)}°C`;
-          maxtemparature.textContent = `Max Temperature: ${maxtemp.toFixed(0)}°C`;
-          mintemparature.textContent = `Min Temperature: ${mintemp.toFixed(0)}°C`;
-          howcloudy.textContent = `${data.weather[0].description}`;
-          wind.textContent = `Wind speed:${data.wind.speed}m/s`;
-          weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-          weatherIcon.style.height = "70px";
-          sunrise.textContent = `Sun Rise : ${new Date(data.sys.sunrise * 1000).toLocaleTimeString()}`;
-          sunset.textContent = `Sun Set : ${new Date(data.sys.sunset * 1000).toLocaleTimeString()}`;
-         }
-      })
-  }
+    .then((data) => {
+      h3.textContent = "";
+      if (data.cod === "404") {
+        h3.textContent = `Entered '${userCity.value}' ${data.message}`;
+      } else {
+        city.textContent = data.name;
+        const actualTemp = data.main.temp - 273;
+        const maxtemp = data.main.temp_max - 273;
+        const mintemp = data.main.temp_min - 273;
+        temperature.textContent = `Temperature: ${actualTemp.toFixed(0)}°C`;
+        maxtemparature.textContent = `Max Temperature: ${maxtemp.toFixed(0)}°C`;
+        mintemparature.textContent = `Min Temperature: ${mintemp.toFixed(0)}°C`;
+        howcloudy.textContent = `${data.weather[0].description}`;
+        wind.textContent = `Wind speed:${data.wind.speed}m/s`;
+        weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+        weatherIcon.style.height = "70px";
+        sunrise.textContent = `Sun Rise : ${new Date(
+          data.sys.sunrise * 1000
+        ).toLocaleTimeString()}`;
+        sunset.textContent = `Sun Set : ${new Date(
+          data.sys.sunset * 1000
+        ).toLocaleTimeString()}`;
+      }
+    });
+}

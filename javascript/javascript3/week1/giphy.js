e0bb4d74cdb596f4194f5c2b37f55982b47a6556
thathinisai <thathinisai@gmail.com>
2021-10-06 14:24:13 +0200

    giphy js file added

--- javascript/javascript3/week1/giphy.js
+++ javascript/javascript3/week1/giphy.js
@@ -4,38 +4,34 @@ const JsNoOfgifs = document.getElementById("NoOfgifs");
 const JsDispGif = document.getElementById("DispGif");
 let url;
 
-function getGiphy()
-{
-    fetch(url)
-    .then(response => response.json())
-    .then(gifData =>
-        {
-        for(const element of gifData.data)
-        {
-            const image=document.createElement('img');
-            const NextLine=document.createElement('br')
-            image.src=element.images.downsized_still.url;
-            JsDispGif.appendChild(image)
-            JsDispGif.appendChild(NextLine)
-        }
-        });
+function getGiphy() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((gifData) => {
+      for (const element of gifData.data) {
+        const image = document.createElement("img");
+        const NextLine = document.createElement("br");
+        image.src = element.images.downsized_still.url;
+        JsDispGif.appendChild(image);
+        JsDispGif.appendChild(NextLine);
+      }
+    });
 }
 
-function getGiphyAll()
-{
-    JsDispGif.innerHTML="";
-    const inputSearchValue=JsUserInput.value
-    url=`https://api.giphy.com/v1/gifs/search?api_key=ihXo5hIlRPNuopy1YYYtD28Eow9RNkO1&q=${inputSearchValue}&limit=25`
-    getGiphy(); 
+function getGiphyAll() {
+  JsDispGif.innerHTML = "";
+  const inputSearchValue = JsUserInput.value;
+  url = `https://api.giphy.com/v1/gifs/search?api_key=ihXo5hIlRPNuopy1YYYtD28Eow9RNkO1&q=${inputSearchValue}&limit=25`;
+  getGiphy();
 }
 
-function getGiphyWithNumber(){
-    JsDispGif.innerHTML="";
-    const inputSearchValue=JsUserInput.value
-    let numberOfGifs=JsNoOfgifs.value
-url=`https://api.giphy.com/v1/gifs/search?api_key=ihXo5hIlRPNuopy1YYYtD28Eow9RNkO1&q=${inputSearchValue}&limit=${numberOfGifs}`
- getGiphy();
+function getGiphyWithNumber() {
+  JsDispGif.innerHTML = "";
+  const inputSearchValue = JsUserInput.value;
+  let numberOfGifs = JsNoOfgifs.value;
+  url = `https://api.giphy.com/v1/gifs/search?api_key=ihXo5hIlRPNuopy1YYYtD28Eow9RNkO1&q=${inputSearchValue}&limit=${numberOfGifs}`;
+  getGiphy();
 }
 
-JsButton.addEventListener('click',getGiphyAll)
-JsNoOfgifs.addEventListener('input',getGiphyWithNumber)
+JsButton.addEventListener("click", getGiphyAll);
+JsNoOfgifs.addEventListener("input", getGiphyWithNumber);
